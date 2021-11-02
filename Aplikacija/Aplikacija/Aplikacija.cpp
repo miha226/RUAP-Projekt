c9223e3636491e0b4359e87bc683db9feb00e1f8
Nikola <nstipic@etfos.hr>
2021-11-02 17:27:46 +0100

    Prva verzija

--- Aplikacija/Aplikacija/Aplikacija.cpp
+++ Aplikacija/Aplikacija/Aplikacija.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Pozdrav iz aplikacije od Michaela Srempfa, broj indeksa 123123123\n";
+    std::cout << "Pozdrav iz aplikacije od Michaela Srempfa\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
